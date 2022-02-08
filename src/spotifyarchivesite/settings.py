999153e6e052e7955846b8267db9ee2e4df8117b
sen-francis <s3nfrancis@gmail.com>
2022-02-08 00:00:44 -0500

    Hid django secret.

--- src/spotifyarchivesite/settings.py
+++ src/spotifyarchivesite/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.cfg')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f&tvbcbpods!omtf(!9tgnplhqh&k^b#oy*-^1h@fnu8+6ukxk'
+SECRET_KEY = config.get('DJANGO', 'DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
