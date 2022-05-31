22816d7b2aaca617de99ea1143610f7dcd90e1c0
sen-francis <s3nfrancis@gmail.com>
2022-05-31 03:09:08 -0500

    Song selection implemented. Recommendations displayed.

--- src/spotifyarchiveapp/urls.py
+++ src/spotifyarchiveapp/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls import handler404
 urlpatterns = [
     path('', views.home, name='home'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('playlist/', views.playlist, name='playlist'),
 ]
 
 handler404 = 'spotifyarchiveapp.views.error404'
--- src/spotifyarchiveapp/views.py
+++ src/spotifyarchiveapp/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 import spotipy
 import spotipy.util as util
 from spotipy import oauth2
+import os
 
 config = configparser.ConfigParser()
 config.read('config.cfg')
@@ -17,8 +18,10 @@ CACHE = '.spotipyoauthcache'
 
 sp_oauth = oauth2.SpotifyOAuth(
 	client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET,
-	redirect_uri=SPOTIPY_REDIRECT_URI, scope=SCOPE, cache_path=CACHE
+	redirect_uri=SPOTIPY_REDIRECT_URI, scope=SCOPE, cache_path=CACHE, 
+    show_dialog=True
 )
+
 sp = spotipy.Spotify(auth_manager=sp_oauth)
 
 def error404(request, exception):
@@ -31,6 +34,17 @@ def home(request):
     return render(request, 'spotifyarchiveapp/home.html')
 
 def dashboard(request):
+    if(request.GET.get('code')):
+        token = sp_oauth.get_access_token(request.GET.get('code'))
+    if('logout' in request.POST):
+        os.remove('.spotipyoauthcache')
+        try:
+            del request.session['selected-tracks']
+        except KeyError:
+            pass
+        return render(request, 'spotifyarchiveapp/home.html')
+    if('submitButton' in request.POST):
+        return redirect(playlist)
     user = sp.current_user()
     args = {}
     args["display_name"] = user["display_name"]
@@ -39,7 +53,40 @@ def dashboard(request):
     if(query):
         results = sp.search(q=query, limit=10, type='track')
         args["results"]=results
-        #return redirect(reverse('spotifyarchiveapp/dashboard.html'), args)
-        return render(request, 'spotifyarchiveapp/dashboard.html', args)
+    #if(request.method == 'POST'):
+    if('trackSelect' in request.POST):
+        trackID = request.POST.get("track-id")
+        track = sp.track(track_id=trackID)
+        if('selected-tracks' in request.session):
+            if(track not in request.session['selected-tracks']):
+                request.session['selected-tracks'].insert(0,track)
+                if(len(request.session['selected-tracks']) > 5):
+                    request.session['selected-tracks'].pop()
+        else:
+            request.session['selected-tracks'] = [track]
+        request.session.modified = True
+    if('selected-tracks' in request.session):
+        args["selected"]= request.session['selected-tracks']
     return render(request, 'spotifyarchiveapp/dashboard.html', args)
 
+def playlist(request):
+    print('HERE')
+    if('logout' in request.POST):
+        os.remove('.spotipyoauthcache')
+        try:
+            del request.session['selected-tracks']
+        except KeyError:
+            pass
+        return render(request, 'spotifyarchiveapp/home.html')
+    #if('createButton' in request.POST):
+        #create playlist
+    user = sp.current_user()
+    args = {}
+    args["display_name"] = user["display_name"]
+    args["avi_url"] = user["images"][0]["url"]
+    tracks = []
+    for track in request.session['selected-tracks']:
+        tracks.append(track["id"])
+    recs = sp.recommendations(seed_tracks=tracks, limit=100)
+    args["results"] = recs['tracks']
+    return render(request, 'spotifyarchiveapp/playlist.html', args)
